030f249a035a6ac723e244baecdeedfe3443ad93
Lee Seok Joon <seokjoon@gmail.com>
2018-02-13 16:33:09 +0900

    naming fix

---
+++ components/com_ecfirmnettopic/dispatcher.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package ecfirm.net
+ * @copyright Copyright (C) ecfirm.net. All rights reserved.
+ * @license GNU General Public License version 2 or later.
+ */
+
+use Joomla\CMS\Dispatcher\Dispatcher;
+
+defined('_JEXEC') or die;
+
+class EcfirmNetTopicDispatcher extends Dispatcher
+{
+
+	/**
+	 * The extension namespace
+	 *
+	 * @var    string
+	 *
+	 * @since  4.0.0
+	 */
+	protected $namespace = 'Joomla\\Component\\EcfirmNetTopic';
+}
---
+++ components/com_ecfirmnettopic/tmpl/topics/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package ecfirm.net
+ * @copyright Copyright (C) ecfirm.net. All rights reserved.
+ * @license GNU General Public License version 2 or later.
+ */
+
+defined('_JEXEC') or die;
