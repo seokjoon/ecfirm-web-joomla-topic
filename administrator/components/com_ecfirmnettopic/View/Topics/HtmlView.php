e3856b84e3d9b087ac14aa84ecabfaaddcf111f2
seokjoon <seokjoon@gmail.com>
2018-03-02 23:13:42 +0900

    admin: topics views

--- administrator/components/com_ecfirmnettopic/View/Topics/HtmlView.php
+++ administrator/components/com_ecfirmnettopic/View/Topics/HtmlView.php
@@ -7,10 +7,25 @@
 
 namespace Joomla\Component\EcfirmNetTopic\Administrator\View\Topics;
 
+use Joomla\CMS\Toolbar\ToolbarHelper;
 use Joomla\Component\EcfirmNetBase\Administrator\View\EcListAdminHtmlView;
 
 defined('_JEXEC') or die;
 
 class HtmlView extends EcListAdminHtmlView
 {
+
+	protected function addToolbar()
+	{
+		parent::addToolbar();
+		//$user = Factory::getUser();
+		//$bar = Toolbar::getInstance('toolbar');
+		$canDo = $this->canDo;
+		if ($canDo->get('core.create'))
+			ToolbarHelper::addNew('topic.add');
+		if (($canDo->get('core.edit')) || ($canDo->get('core.edit.own')))
+			ToolbarHelper::editList('topic.edit');
+		if ($canDo->get('core.delete'))
+			ToolbarHelper::deleteList('', 'topics.delete', 'COM_ECTOPIC_DELETE');
+	}
 }
--- administrator/components/com_ecfirmnettopic/tmpl/topics/default.php
+++ administrator/components/com_ecfirmnettopic/tmpl/topics/default.php
@@ -5,8 +5,95 @@
  * @license GNU General Public License version 2 or later.
  */
 
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
 use Joomla\Component\EcfirmNetBase\Site\Helper\EcDebug;
 
 defined('_JEXEC') or die;
 
-EcDebug::lp(__FILE__);
+//HTMLHelper::_('bootstrap.tooltip');
+HTMLHelper::_('behavior.multiselect');
+HTMLHelper::_('formbehavior.chosen', 'select');
+
+$urlForm = Route::_(Uri::getInstance());
+
+$columns = 7; //FIXME
+
+//EcDebug::lp($this->items);
+?>
+
+
+
+<form action="<?php echo $urlForm; ?>" method="post" name="adminForm" id="adminForm">
+	<div class="row">
+		<?php if(!empty($this->sidebar)) : ?>
+		<div id="j-sidebar-container" class="col-md-2">
+			<?php echo $this->sidebar; ?>
+		</div>
+		<?php endif; ?>
+
+		<div class="<?php if (!empty($this->sidebar)) echo 'col-md-10'; else echo 'col-md-12'; ?>">
+			<div id="j-main-container" class="j-main-container">
+
+				<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
+
+				<?php if (empty($this->items)) : ?>
+				<joomla-alert type="warning">
+					<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+				</joomla-alert>
+
+				<?php else : ?>
+				<table class="table table-striped" id="categoryList">
+					<thead>
+						<tr>
+							<th style="width: 1%" class="text-center">
+								<?php echo HTMLHelper::_('grid.checkall'); ?>
+							</th>
+							<th class="text-center">
+								<?php echo Text::_('COM_ECTOPIC_TOPIC_TITLE_HEADER'); ?>
+							</th>
+							<th class="text-center">
+								<?php echo Text::_('COM_ECTOPIC_TOPIC_USER_HEADER'); ?>
+							</th>
+						</tr>
+					</thead>
+
+					<tfoot>
+					<tr>
+						<td colspan="<?php echo $columns; ?>">
+							<?php echo $this->pagination->getListFooter(); ?>
+						</td>
+					</tr>
+					</tfoot>
+
+					<tbody>
+						<?php foreach ($this->items as $i => $item) : ?>
+						<tr class="row<?php echo ($i % 2)?>" sortable-group-id="<?php echo $item->topic; ?>">
+							<td class="text-center">
+								<?php echo HTMLHelper::_('grid.id', $i, $item->topic); ?>
+							</td>
+							<td class="">
+								<a href="<?php echo Route::_('index.php?option=com_ectopic&task=topic.edit&topic=').$item->topic; ?>" title="<?php echo Text::_('JACTION_EDIT'); ?>"><?php echo $item->title; ?></a>
+							</td>
+							<td class="text-center">
+								<?php echo $item->username; ?>
+							</td>
+						</tr>
+
+						<?php endforeach; ?>
+					</tbody>
+
+				</table>
+				<?php endif; ?>
+
+				<input type="hidden" name="task" value="">
+				<input type="hidden" name="boxchecked" value="0">
+				<?php echo HTMLHelper::_('form.token'); ?>
+
+			</div>
+		</div>
+	</div>
+</form>
